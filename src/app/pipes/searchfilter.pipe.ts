ab4855ed0607da8e13f2b13594f10ae74d9fffc1
mikejunt <mikejunt@gmail.com>
2020-02-12 14:34:54 -0600

    search functionality

--- src/app/pipes/searchfilter.pipe.ts
+++ src/app/pipes/searchfilter.pipe.ts
@@ -5,8 +5,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SearchfilterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(todoList: Array<Object>, searchString: string): Array<Object> {
+    searchString = searchString.toLowerCase()
+    let searchresult: Array<Object> = todoList.filter(obj => {
+        return obj["user"].toLowerCase().includes(searchString) ||  obj["name"].toLowerCase().includes(searchString) 
+    })
+    return searchresult
   }
-
 }
