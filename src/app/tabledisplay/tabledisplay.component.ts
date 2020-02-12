c558cff491be026949009b15daf9a6cbeff691be
mikejunt <mikejunt@gmail.com>
2020-02-12 13:49:44 -0600

    reading search selectors

--- src/app/tabledisplay/tabledisplay.component.ts
+++ src/app/tabledisplay/tabledisplay.component.ts
@@ -7,10 +7,12 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class TabledisplayComponent implements OnInit {
   searchtext: string = ""
+  searchtype: string = ""
   test = 5
 @Input('todolist') todolist
 @Output() complete = new EventEmitter<number>()
 markedComp() {this.complete.emit(this.test)}
+initSearch() {console.log(this.searchtext, this.searchtype)}
   constructor() { }
   ngOnInit(): void {
   }
